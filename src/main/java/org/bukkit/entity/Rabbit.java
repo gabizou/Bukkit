0e7d112f9c6274f4de3a9587dca622155e75d85d
Gabriel Harris-Rouquette <gabizou@me.com>
2014-10-01 22:58:00 -0700

    Remove magic numbers from RabbitType

--- src/main/java/org/bukkit/entity/Rabbit.java
+++ src/main/java/org/bukkit/entity/Rabbit.java
@@ -28,53 +28,12 @@ public interface Rabbit extends Animals
     */
     public enum RabbitType
     {
-        BROWN(0),
-        WHITE(1),
-        BLACK(2),
-        BLACK_AND_WHITE(3),
-        GOLD(4),
-        SALT_PEPPER(5),
-        KILLER(99);
-
-        private static final Map<Integer, RabbitType> types = new HashMap<Integer, RabbitType>(values().length);
-        private final int id;
-
-        static
-        {
-            for (RabbitType type : values())
-            {
-                types.put(type.id, type);
-            }
-        }
-
-        private RabbitType(int id)
-        {
-            this.id = id;
-        }
-
-        /**
-         * Gets the ID of this rabbit type.
-         *
-         * @return Rabbit type ID
-         * @deprecated Magic value
-         */
-        @Deprecated
-        public int getId()
-        {
-            return id;
-        }
-
-        /**
-         * Gets a rabbit type by its ID.
-         *
-         * @param id ID of the rabbit type to get.
-         * @return Resulting rabbit type, or null if not found.
-         * @deprecated Magic value
-         */
-        @Deprecated
-        public static RabbitType getType(int id)
-        {
-            return types.get(id);
-        }
+        BROWN,
+        WHITE,
+        BLACK,
+        BLACK_AND_WHITE,
+        GOLD,
+        SALT_PEPPER,
+        KILLER
     }
 }
